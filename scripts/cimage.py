67b09dc749bca0d8c1eca5f94fb13d99c450baf1
MAYANK AGRAWAL <31409167+wolwire@users.noreply.github.com>
2024-01-14 04:10:02 +0530

    Update cimage.py

--- scripts/cimage.py
+++ scripts/cimage.py
@@ -5,5 +5,5 @@ def convert_to_sd(img):
     shapes = []
     chunks = detect(img)
     for chunk in chunks:
-        shapes.append(false)
+        shapes.append(False)
     return [any(shapes), tempfile.NamedTemporaryFile(delete=False, suffix=".png")]
